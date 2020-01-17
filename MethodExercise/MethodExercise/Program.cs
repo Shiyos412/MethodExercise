a63cb12b28b75168ac4136dfd52ec2e179c42996
Michael Cerase <michaelcerase@gmail.com>
2020-01-17 10:33:37 -0600

    Reading console lines

---
+++ MethodExercise/MethodExercise/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MethodExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("What is your age?");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Name: " + name + "\n" + "Age: " + age);
+        }
+    }
+}
